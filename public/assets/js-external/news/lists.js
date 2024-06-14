109118eb55bf821fe836471d827c39274ffb1855
Tapakron <tapakron.po@gmail.com>
2024-06-14 16:01:28 +0700

    cm

--- public/assets/js-external/news/lists.js
+++ public/assets/js-external/news/lists.js
@@ -116,6 +116,38 @@ var KTModalNewTicket = function () {
         }
     }
 }();
+var Daterangepicker = function () {
+    return {
+        init: function () {
+            var start = moment().subtract(29, "days");
+            var end = moment();
+
+            function cb(start, end) {
+                $("#kt_daterangepicker_4").html(start.format("MMMM D, YYYY") + " - " + end.format("MMMM D, YYYY"));
+            }
+
+            $("#kt_daterangepicker_4").daterangepicker({
+                startDate: start,
+                endDate: end,
+                ranges: {
+                    "Today": [moment(), moment()],
+                    "Yesterday": [moment().subtract(1, "days"), moment().subtract(1, "days")],
+                    "Last 7 Days": [moment().subtract(6, "days"), moment()],
+                    "Last 30 Days": [moment().subtract(29, "days"), moment()],
+                    "This Month": [moment().startOf("month"), moment().endOf("month")],
+                    "Last Month": [moment().subtract(1, "month").startOf("month"), moment().subtract(1, "month").endOf("month")]
+                },
+                locale: {
+                    format: 'DD/MM/YYYY'
+                }
+            }, cb);
+
+            cb(start, end);
+        }
+    }
+}();
 KTUtil.onDOMContentLoaded((function () {
     KTModalNewTicket.init()
+    Daterangepicker.init()
+    // $("#kt_daterangepicker_4").daterangepicker();
 }));
--- resources/views/pages/news/lists.blade.php
+++ resources/views/pages/news/lists.blade.php
@@ -6,72 +6,39 @@
 @section('content')
 {{-- !------------------------------------------------------- --}}
 <div class="card mb-12">
-    <!--begin::Hero body-->
     <div class="card-body flex-column p-5">
-        <!--begin::Hero content-->
         <div class="d-flex align-items-center h-lg-100px p-5">
-            <!--begin::Wrapper-->
             <div class="d-flex flex-column align-items-start justift-content-center flex-equal me-5">
-                <!--begin::Input group-->
                 <div class="position-relative w-100">
                     <i class="ki-outline ki-magnifier fs-2 text-primary position-absolute top-50 translate-middle ms-8"></i>
                     <input type="text" class="form-control fs-4 py-4 ps-14 text-gray-700 placeholder-gray-400 mw-500px" name="search" value="" placeholder="ค้นหาข่าวสาร" />
                 </div>
-                <!--end::Input group-->
             </div>
-            <!--end::Wrapper-->
-            <!--begin::Wrapper-->
             <div class="flex-equal d-flex justify-content-center align-items-end ms-5">
-                <input class="form-control fs-4 py-4 ps-14 text-gray-700 placeholder-gray-400 mw-500px" placeholder="05/07/2024 - 05/13/2024" id="kt_daterangepicker_4"/>
-                <!--begin::Primary button-->
-                <!-- <a href="#" class="btn fw-bold btn-primary ms-5">ค้นหา</a> -->
-                <!--end::Primary button-->
+                <input class="form-control fs-4 py-4 ps-14 text-gray-700 placeholder-gray-400 mw-500px" placeholder="" id="kt_daterangepicker_4" value="06/01/2024 - 06/30/2024"/>
             </div>
-            <!--end::Wrapper-->
             <button class="btn btn-primary ms-5">ค้นหา</button>
         </div>
-        <!--end::Hero content-->
     </div>
-    <!--end::Hero body-->
 </div>
 <div class="card">
-    <!--begin::Card body-->
     <div class="card-body">
-        <!--begin::Layout-->
         <div class="row p-7">
-            <!--begin::Tickets-->
-            <!--begin::Heading-->
             <div class="card-rounded d-flex flex-stack flex-wrap p-5">
                 <h1 class="text-dark">รายการข่าวสาร</h1>
-                <!--begin::Action-->
                 <a href="#" data-bs-toggle="modal" data-bs-target="#kt_modal_new_ticket" class="btn btn-light-primary fw-bold fs-8 fs-lg-base mb-2">เพิ่มข่าวสาร</a>
-                <!--end::Action-->
             </div>
-            <!--end::Heading-->
-            <!--begin::Tickets List-->
             <div class="mb-10">
-                <!--begin::Ticket-->
+                @for ($i = 0; $i < 8; $i++)
                 <div class="d-flex mb-10">
-                    <!--begin::Symbol-->
                     <i class="ki-outline ki-file-added fs-2x me-5 ms-n1 mt-2 text-success"></i>
-                    <!--end::Symbol-->
-                    <!--begin::Section-->
                     <div class="d-flex flex-column">
-                        <!--begin::Content-->
                         <div class="d-flex align-items-center mb-2">
-                            <!--begin::Title-->
                             <a href="{{ url("/news/detail/") }}" class="text-dark text-hover-primary fs-4 me-3 fw-semibold">ตารางโปรโมชันลูกค้าเดือนพฤษภาคม</a>
-                            <!--end::Title-->
                         </div>
-                        <!--end::Content-->
-                        <!--begin::Text-->
                         <span class="text-gray-700 fw-semibold fs-6">Keenthemes example description</span>
-                        <!--end::Text-->
-                        <!--begin::time-->
                         <span class="text-muted fw-semibold fs-7 mt-2">20/05/2024 13:00 น.</span>
-                        <!--end::time-->
                     </div>
-                    <!--end::Section-->
                     <span class="flex-equal d-flex justify-content-center ms-5">
                         <a href="#" class="btn btn-icon btn-light w-30px h-30px ms-auto" data-bs-toggle="modal" data-bs-target="#kt_modal_new_ticket">
                             <i class="ki-duotone ki-pencil fs-2 ms-0">
@@ -93,10 +60,8 @@
                         </span>
                     </span>
                 </div>
-                <!--end::Ticket-->
+                @endfor
             </div>
-            <!--end::Tickets List-->
-            <!--begin::Pagination-->
             <ul class="pagination">
                 <li class="page-item previous disabled">
                     <a href="#" class="page-link">
@@ -127,49 +92,30 @@
                     </a>
                 </li>
             </ul>
-            <!--end::Pagination-->
-            <!--end::Tickets-->
-            
         </div>
-        <!--end::Layout-->
     </div>
-    <!--end::Card body-->
 </div>
 {{-- !------------------------------------------------------- --}}
 @endsection
 
 @section('modal-content')
 <div class="modal fade" id="kt_modal_new_ticket" tabindex="-1" aria-hidden="true">
-    <!--begin::Modal dialog-->
     <div class="modal-dialog modal-dialog-centered mw-750px">
-        <!--begin::Modal content-->
         <div class="modal-content rounded">
-            <!--begin::Modal header-->
             <div class="modal-header pb-0 border-0 justify-content-end">
-                <!--begin::Close-->
                 <div class="btn btn-sm btn-icon btn-active-color-primary" data-bs-dismiss="modal">
                     <i class="ki-duotone ki-cross fs-1">
                         <span class="path1"></span>
                         <span class="path2"></span>
                     </i>
                 </div>
-                <!--end::Close-->
             </div>
-            <!--begin::Modal header-->
-            <!--begin::Modal body-->
             <div class="modal-body scroll-y px-10 px-lg-15 pt-0 pb-15">
-                <!--begin:Form-->
                 <form id="kt_modal_new_ticket_form" class="form" action="#">
-                    <!--begin::Heading-->
                     <div class="mb-13 text-center">
-                        <!--begin::Title-->
                         <h1 class="mb-3">เพิ่มข่าวสาร</h1>
-                        <!--end::Title-->
                     </div>
-                    <!--end::Heading-->
-                    <!--begin::Input group-->
                     <div class="d-flex flex-column mb-8 fv-row">
-                        <!--begin::Label-->
                         <label class="d-flex align-items-center fs-6 fw-semibold mb-2">
                             <span class="required">หัวข้อ</span>
                             <span class="ms-2" data-bs-toggle="tooltip" title="หัวข้อข่าวสารที่ต้องการแจ้ง">
@@ -180,41 +126,27 @@
                                 </i>
                             </span>
                         </label>
-                        <!--end::Label-->
                         <input type="text" class="form-control form-control-solid" placeholder="กรอกข้อมูล" name="subject" />
                     </div>
-                    <!--end::Input group-->
-                    <!--begin::Input group-->
                     <div class="d-flex flex-column mb-5 fv-row">
                         <label class="fs-6 fw-semibold mb-2">รายละเอียดข่าวสาร</label>
                         <textarea class="form-control form-control-solid" rows="4" name="description" placeholder="กรอกรายละเอียดข่าวสาร"></textarea>
                     </div>
-                    <!--end::Input group-->
-                    <!--begin::Input group-->
                     <div class="fv-row mb-8">
                         <label class="fs-6 fw-semibold mb-2">อัปโหลดรูปภาพ</label>
-                        <!--begin::Dropzone-->
                         <div class="dropzone" id="kt_modal_create_ticket_attachments">
-                            <!--begin::Message-->
                             <div class="dz-message needsclick align-items-center">
-                                <!--begin::Icon-->
                                 <i class="ki-duotone ki-file-up fs-3hx text-primary">
                                     <span class="path1"></span>
                                     <span class="path2"></span>
                                 </i>
-                                <!--end::Icon-->
-                                <!--begin::Info-->
                                 <div class="ms-4">
                                     <h3 class="fs-5 fw-bold text-gray-900 mb-1">อัปโหลดรูปภาพ</h3>
                                     <span class="fw-semibold fs-7 text-gray-400">ขนาดไฟล์ไม่เกิน 5MB</span>
                                 </div>
-                                <!--end::Info-->
                             </div>
                         </div>
-                        <!--end::Dropzone-->
                     </div>
-                    <!--end::Input group-->
-                    <!--begin::Actions-->
                     <div class="text-center">
                         <button type="reset" id="kt_modal_new_ticket_cancel" class="btn btn-light me-3">ยกเลิก</button>
                         <button type="submit" id="kt_modal_new_ticket_submit" class="btn btn-primary">
@@ -223,18 +155,14 @@
                             <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
                         </button>
                     </div>
-                    <!--end::Actions-->
                 </form>
-                <!--end:Form-->
             </div>
-            <!--end::Modal body-->
         </div>
-        <!--end::Modal content-->
     </div>
-    <!--end::Modal dialog-->
 </div>
 @endsection
 
 @section('js-content')
-<script src="{{ asset("assets/js/custom/apps/support-center/tickets/create.js") }}"></script>
+{{-- <script src="{{ asset("assets/js/custom/apps/support-center/tickets/create.js") }}"></script> --}}
+<script src="{{ asset("assets/js-external/news/lists.js") }}"></script>
 @endsection
