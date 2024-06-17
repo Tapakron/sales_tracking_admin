98fcaa7dde505a5f8010ed66277a7180c30d7d7d
Pack-260836 <prapan.260836@gmail.com>
2024-06-17 12:49:34 +0700

    Update NewsModel.php

--- app/Models/CompanyModels/NewsModel.php
+++ app/Models/CompanyModels/NewsModel.php
@@ -58,7 +58,7 @@ class NewsModel
                 ->whereDate('created_at', '<=', $fliters['date_end']);
         }
 
-        $newsList = $query->paginate(2); // Adjust the number of items per page as needed
+        $newsList = $query->paginate(1); // Adjust the number of items per page as needed
 
         return $newsList;
     }
