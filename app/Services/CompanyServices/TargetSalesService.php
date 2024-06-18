9aceb905c0899ec689fd6a1494be305f3772440d
Pack-260836 <prapan.260836@gmail.com>
2024-06-18 12:03:14 +0700

    Update TargetSalesService.php

--- app/Services/CompanyServices/TargetSalesService.php
+++ app/Services/CompanyServices/TargetSalesService.php
@@ -20,46 +20,48 @@ class TargetSalesService
             $user = Auth::user();
             $tarGetSales = array();
             $commissionSales = array();
-            if (!array_key_exists("targetsales_id", $body)) {
-                $targetsale_id = GlobalFunc::getNewId();
-                $tarGetSales = [
-                    'targetsales_id' => $targetsale_id,
-                    'company_id' => $user->company_id,
-                    'sales_target' => $body['targetValue'],
-                    'is_delete' => false,
-                    'created_at' => Carbon::now(),
-                    'created_by' => $user->id
-                ];
-                TargetSalesModel::create($tarGetSales);
-            } else {
-                $targetsale_id = $body['targetsales_id'];
-                $tarGetSales = [
-                    'sales_target' => $body['targetValue'],
-                    'updated_at' => Carbon::now(),
-                    'updated_by' => $user->id
-                ];
-                TargetSalesModel::update($targetsale_id, $tarGetSales);
-                CommissionModel::delete($targetsale_id);
-            }
-
-            foreach ($body['commissionItems'] as $key => $value) {
-                $commissionSales[$key]['targetsales_id'] = $targetsale_id;
-                $commissionSales[$key]['commission'] = $value['percent'];
-                $commissionSales[$key]['first_sales'] = $value['start_sales'];
-                $commissionSales[$key]['top_sales'] = $value['max_sales'];
-                $commissionSales[$key]['created_at'] =  Carbon::now();
-                $commissionSales[$key]['created_by'] = $user->id;
-            }
-            $rsCreate = CommissionModel::create($commissionSales);
-            if ($rsCreate == false) {
-                $rs['message'] = "บันทึกข้อมูลผิดพลาด";
+            if (!empty($body['commissionItems'])) {
+                if (!array_key_exists("targetsales_id", $body)) {
+                    $targetsale_id = GlobalFunc::getNewId();
+                    $tarGetSales = [
+                        'targetsales_id' => $targetsale_id,
+                        'company_id' => $user->company_id,
+                        'sales_target' => $body['targetValue'],
+                        'is_delete' => false,
+                        'created_at' => Carbon::now(),
+                        'created_by' => $user->id
+                    ];
+                    TargetSalesModel::create($tarGetSales);
+                } else {
+                    $targetsale_id = $body['targetsales_id'];
+                    $tarGetSales = [
+                        'sales_target' => $body['targetValue'],
+                        'updated_at' => Carbon::now(),
+                        'updated_by' => $user->id
+                    ];
+                    TargetSalesModel::update($targetsale_id, $tarGetSales);
+                    CommissionModel::delete($targetsale_id);
+                }
+                foreach ($body['commissionItems'] as $key => $value) {
+                    $commissionSales[$key]['targetsales_id'] = $targetsale_id;
+                    $commissionSales[$key]['commission'] = $value['percent'];
+                    $commissionSales[$key]['first_sales'] = $value['start_sales'];
+                    $commissionSales[$key]['top_sales'] = $value['max_sales'];
+                    $commissionSales[$key]['created_at'] =  Carbon::now();
+                    $commissionSales[$key]['created_by'] = $user->id;
+                }
+                $rsCreate = CommissionModel::create($commissionSales);
+                if ($rsCreate == false) {
+                    $rs['message'] = "บันทึกข้อมูลผิดพลาด";
+                    $rs['success'] = $rsCreate;
+                    DB::commit();
+                    return $rs;
+                }
+                $rs['message'] = "บันทึกข้อมูลสำเร็จ";
                 $rs['success'] = $rsCreate;
-                DB::commit();
-                return $rs;
             }
-            $rs['message'] = "บันทึกข้อมูลสำเร็จ";
-            $rs['success'] = $rsCreate;
-
+            $rs['message'] = "โปรดระบุค่าคอมมิชชั่นอย่างน้อย 1 รายการ";
+            $rs['success'] = false;
             DB::commit();
             return $rs;
         } catch (\Throwable $th) {
